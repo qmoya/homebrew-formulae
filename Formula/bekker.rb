44b588028898650cb1f37c8faf467399a4400477
Quico Moya <me@qmoya.com>
2020-07-24 13:59:12 +0200

    Add formula for bekker

---
+++ Formula/bekker.rb
@@ -0,0 +1,15 @@
+class Bekker < Formula
+  desc "A Perseus-XML-to-Roam-Research converter"
+  homepage "https://github.com/qmoya/Bekker"
+  head "https://github.com/qmoya/Bekker", branch => :master
+
+  depends_on "unibetacode" => :build
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/bekker"
+  end
+end
